28316c324652f791eb48dfece40fa25fc3da4a68
_@Nazib_07 <nazib9007.ice.bup@gmail.com>
2026-03-25 23:53:30 +0600

    Update index.js
    Added JavaScript prototypal inheritance example with Teacher object.

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ class Car {
 }
 let car1 = new Car ('Tayota', 'T23O98');
 console.log(car1.name +' '+car1.model);
+
+//JavaScript Prototypal Inheritance-->
+//Prototypal Inheritance মানে "একটি অবজেক্ট আরেকটি অবজেক্ট থেকে প্রপার্টি/মেথড নেওয়া"। যেমন সন্তান বাবা-মায়ের থেকে গুণাবলী নেয়।
+let Teacher = {
+    name: 'Karim',
+    Age: 45,
+    Id: 8989,  
+    teacherPanal (){
+        console.log(`Teacher ${this.name}, his age is ${this.Age} and his id is ${this.Id}`);
+    }
+};
+
+let callTeacher = Object.create(Teacher);
+callTeacher.name = 'Rahim';
+console.log(callTeacher.name);
+console.log(callTeacher.Age);
+console.log(callTeacher.Id);
+callTeacher.teacherPanal();
